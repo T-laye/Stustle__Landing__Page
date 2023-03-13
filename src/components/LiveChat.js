a1344762c84d6cb4da4b17a0eb8be8ef635bf06f
Laye <tlayefaah@gmail.com>
2023-03-13 15:22:20 +0100

    removed little mistake

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -4,14 +4,8 @@ import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 export default function App() {
   const tawkMessengerRef = useRef();
 
-  const handleMinimize = () => {
-    tawkMessengerRef.current.minimize();
-  };
-
   return (
     <div className="App mt">
-      <button onClick={handleMinimize}> Minimize the Chat </button>
-
       <TawkMessengerReact
         propertyId="640f16a231ebfa0fe7f2456c"
         widgetId="1grdf0rhm"
