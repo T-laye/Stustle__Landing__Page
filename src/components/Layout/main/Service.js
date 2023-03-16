0ecf3c3c63dc2624ab9de292ad8f0f169288e876
Laye <tlayefaah@gmail.com>
2023-03-16 17:47:39 +0100

    contact icons integrated

--- src/components/Layout/main/Service.js
+++ src/components/Layout/main/Service.js
@@ -1,7 +1,5 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable jsx-a11y/alt-text */
 import ContactModal from '@/components/ContactModal';
-import Image from 'next/image';
+
 import Link from 'next/link';
 import { useState } from 'react';
 import serviceDetails from '../../serviceDetails';
@@ -16,14 +14,11 @@ export default function Service() {
   const renderService = serviceDetails.map((serv, i) => {
     return (
       <Link href="/#service" key={i} onClick={open}>
-        <div className="w-36  px-4 py-8 mx-auto  h-48 bg-gray-0 flex flex-col justify-start items-start rounded-lg hover:shadow-md hover:bg-white hover:duration-700 xl:scale-150 lg:scale-110 xl:mt-14  ">
-          <div className="relative h-8 w-8 ">
-            <div className="icon">
-              <Image src={serv.icon} alt="icons" />
-            </div>
-            <div className="bk"></div>
+        <div className="w-36  px-4 pt-4 pb-8 mx-auto  h-48 bg-gray-0 flex flex-col justify-start items-start rounded-lg hover:shadow-md hover:bg-white hover:duration-700 xl:scale-150 lg:scale-110 xl:mt-14  ">
+          <div className=" w-full flex justify-start  ">
+            <div className="bk text-black">{serv.icon}</div>
           </div>
-          <h4 className="text-sm font-medium">{serv.title}</h4>
+          <h4 className="text-sm font-medium mt-2">{serv.title}</h4>
           <p className="text-xs text-gray-2 leading-4 mt-0 mb-0">
             {serv.description}
           </p>
@@ -36,12 +31,9 @@ export default function Service() {
     <section id="services">
       <div className="flex flex-col items-center">
         <h2>Our Services</h2>
-        <p className="text-center text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
-          magni?
-        </p>
+        <p className="text-center text-lg">The services we offer:</p>
         <div>
-          <div className="flex flex-wrap gap-4 justify-center mt-6 md:grid grid-cols-3 md:gap-6 xl:gap-x-48 xl:gap-y-20 xl:mb-14 lg:gap-x-20 lg:gap-y-10 lg:mt-8 lg:w-5/6 sm:gap-x-2 md:w-6/7 mx-auto">
+          <div className="flex flex-wrap gap-6 justify-center mt-6 md:grid grid-cols-3 md:gap-6 xl:gap-x-48 xl:gap-y-20 xl:mb-14 lg:gap-x-20 lg:gap-y-10 lg:mt-8 lg:w-5/6 sm:gap-x-2 md:w-6/7 mx-auto">
             {renderService}
           </div>
         </div>
