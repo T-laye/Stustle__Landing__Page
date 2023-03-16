af2848067a63925317b30ddd477aa1c68e7edc69
Laye <tlayefaah@gmail.com>
2023-03-16 17:49:41 +0100

    contact icons and links integrated

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -26,7 +26,11 @@ export default function ContactModal(props) {
         <div className="flex flex-col mt-2 justify-evenly h-full text-xl text-center w-10/12">
           <p>How would you like to contact us ?</p>
           <div className="flex flex-col items-center justify-between bg-blak text-white text-base sm:flex-row">
-            <Link href="/form" target="_blank" onClick={close}>
+            <Link
+              href="https://wa.me/message/JVOKBIKMBAITH1"
+              target="_blank"
+              onClick={close}
+            >
               <button className={styles.btnContactSecondary}>WhatsApp</button>
             </Link>
             <Link href="/form" target="_blank " onClick={close}>
