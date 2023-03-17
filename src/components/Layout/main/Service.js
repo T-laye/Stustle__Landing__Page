cc66eeb0c3688636a30a3846d241a2c01157c17f
Laye <tlayefaah@gmail.com>
2023-03-17 11:43:15 +0100

    iphone 7 wahala

--- src/components/Layout/main/Service.js
+++ src/components/Layout/main/Service.js
@@ -14,7 +14,7 @@ export default function Service() {
   const renderService = serviceDetails.map((serv, i) => {
     return (
       <Link href="/#service" key={i} onClick={open}>
-        <div className="w-36  px-4 pt-4 pb-8 mx-auto  h-48 bg-gray-0 flex flex-col justify-start items-start rounded-lg hover:shadow-md hover:bg-white hover:duration-700 xl:scale-150 lg:scale-110 xl:mt-14  ">
+        <div className="w-36  px-4 pt-4 pb-8   h-48 bg-gray-0 flex flex-col justify-start items-start rounded-lg hover:shadow-md hover:bg-white hover:duration-700 xl:scale-150 lg:scale-110 xl:mt-14  ">
           <div className=" w-full flex justify-start  ">
             <div className="bk text-black">{serv.icon}</div>
           </div>
@@ -35,7 +35,7 @@ export default function Service() {
           These are some of the services we render:
         </p>
         <div>
-          <div className="flex flex-wrap gap-6 justify-center mt-6 md:grid grid-cols-3 md:gap-6 xl:gap-x-48 xl:gap-y-20 xl:mb-14 lg:gap-x-20 lg:gap-y-10 lg:mt-8 lg:w-5/6 sm:gap-x-2 md:w-6/7 mx-auto">
+          <div className="max-[380px]:flex-col flex flex-wrap gap-6 justify-center mt-6 md:grid grid-cols-3 md:gap-6 xl:gap-x-48 xl:gap-y-20 xl:mb-14 lg:gap-x-20 lg:gap-y-10 lg:mt-8 lg:w-5/6 sm:gap-x-2 md:w-6/7 mx-auto">
             {renderService}
           </div>
         </div>
